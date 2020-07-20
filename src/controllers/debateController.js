68ccaa1b669ce4f7fb9f07b36f328c676f59cb49
Morgan Sorbaro <msorbaro@gmail.com>
2020-07-20 17:43:56 -0400

    may be about to break again

--- src/controllers/debateController.js
+++ src/controllers/debateController.js
@@ -293,21 +293,21 @@ export const goToNextRoundWithAPI = (req, res) => {
             notification.message = 'Its your turn to debate!';
             notification.userID = acceptedFirstID;
             return notification.save();
+          } else {
+            RequestModel.findById(post.requestID).then((request) => {
+              console.log('found something');
+              console.log(request.requestUsers.length);
+              for (let i = 0; i < request.requestUsers.length; i += 1) {
+                console.log(request.requestUsers[i]);
+                const notification2 = new Notification();
+                notification2.debateID = post._id;
+                notification2.userID = request.requestUsers[i].userID;
+                notification2.type = 'POSTED_DEBATE';
+                notification2.message = `The debate you requested between ${request.person1} and ${request.person2} is posted!`;
+                notification2.save();
+              }
+            });
           }
-          // else {
-          //   RequestModel.findById(post.requestID).then((request) => {
-          //     console.log('found something');
-          //     for (let i = 0; i < request.requestUsers.length; i += 1) {
-          //       console.log(request.requestUsers[i]);
-          //       const notification2 = new Notification();
-          //       notification2.debateID = post._id;
-          //       notification2.userID = request.requestUsers[i].userID;
-          //       notification2.type = 'POSTED_DEBATE';
-          //       notification2.message = `The debate you requested between ${request.person1} and ${request.person2} is posted!`;
-          //       notification2.save();
-          //     }
-          //   });
-          // }
         })
         .catch((error) => {
           res.status(422).json({ error });
